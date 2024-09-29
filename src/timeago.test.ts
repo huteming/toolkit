69e6d73a5f36fdb0eb2a409d08c6bf60662d62a1
huteming <teming.hu@qq.com>
2024-09-29 18:16:46 +0800

    test: 修复测试用例误差

--- src/timeago.test.ts
+++ src/timeago.test.ts
@@ -12,43 +12,44 @@ const year = 1 * 365 * day
 describe('timeago', () => {
   it('刚刚', () => {
     const now = new Date()
-    expect(format(+now - 3 * second)).toBe('just now')
-    expect(format(+now + 3 * second)).toBe('right now')
+    expect(format(+now - 3 * second, { relativeDate: now })).toBe('just now')
+    expect(format(+now + 3 * second, { relativeDate: now })).toBe('right now')
   })
 
   it('分钟', () => {
     const now = new Date()
-    expect(format(+now - 3 * minute)).toBe('3 minutes ago')
-    expect(format(+now + 3 * minute)).toBe('in 3 minutes')
+    expect(format(+now - 3 * minute, { relativeDate: now })).toBe('3 minutes ago')
+    expect(format(+now + 3 * minute, { relativeDate: now })).toBe('in 3 minutes')
   })
 
   it('小时', () => {
     const now = new Date()
-    expect(format(+now - 3 * hour)).toBe('3 hours ago')
-    expect(format(+now + 3 * hour)).toBe('in 3 hours')
+    const diff = 3 * hour
+    expect(format(+now - diff, { relativeDate: now })).toBe('3 hours ago')
+    expect(format(+now + diff, { relativeDate: now })).toBe('in 3 hours')
   })
 
   it('天', () => {
     const now = new Date()
-    expect(format(+now - 3 * day)).toBe('3 days ago')
-    expect(format(+now + 3 * day)).toBe('in 3 days')
+    expect(format(+now - 3 * day, { relativeDate: now })).toBe('3 days ago')
+    expect(format(+now + 3 * day, { relativeDate: now })).toBe('in 3 days')
   })
 
   it('星期', () => {
     const now = new Date()
-    expect(format(+now - 3 * week)).toBe('3 weeks ago')
-    expect(format(+now + 3 * week)).toBe('in 3 weeks')
+    expect(format(+now - 3 * week, { relativeDate: now })).toBe('3 weeks ago')
+    expect(format(+now + 3 * week, { relativeDate: now })).toBe('in 3 weeks')
   })
 
   it('月', () => {
     const now = new Date()
-    expect(format(+now - 3 * month)).toBe('3 months ago')
-    expect(format(+now + 3 * month)).toBe('in 3 months')
+    expect(format(+now - 3 * month, { relativeDate: now })).toBe('3 months ago')
+    expect(format(+now + 3 * month, { relativeDate: now })).toBe('in 3 months')
   })
 
   it('年', () => {
     const now = new Date()
-    expect(format(+now - 3 * year)).toBe('3 years ago')
-    expect(format(+now + 3 * year)).toBe('in 3 years')
+    expect(format(+now - 3 * year, { relativeDate: now })).toBe('3 years ago')
+    expect(format(+now + 3 * year, { relativeDate: now })).toBe('in 3 years')
   })
 })
